a958368bf5771be0cf9572f5867bc0b64abbd83b
Beqir Fazli <57021385+Fazlibeqir@users.noreply.github.com>
2023-08-29 14:52:09 +0200

    skyBoxProblem
    Works in main not in another class !?

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,6 +1,7 @@
 #include "main.hpp"
 
 void handleReturnToMenu(MainMenu &mainMenu) {
+    mainMenu.renderImGui();
     if (GlobalVariables::returnToMenuClicked) {
         mainMenu.gameStarted = false;
         mainMenu.show();
@@ -91,30 +92,10 @@ unsigned int skyboxIndices[] =
                 3, 7, 6,
                 6, 2, 3
         };
-
-int main() {
-    Init init;
-    GLFWwindow *window = Init::initializeWindow();
-    map<const string, string> mapForPaths = Init::initializeShadersAndModelsPaths();
-
-    MainMenu mainMenu(window);
-    mainMenu.initializeImGui();
-    mainMenu.show();
-    Game game(mapForPaths["carVertPath"], mapForPaths["carFragPath"],
-              mapForPaths["carOneModelPath"],
-              mapForPaths["carTwoModelPath"],
-              mapForPaths["carThreeModelPath"],
-              mapForPaths["tyre1ModelPath"],
-              mapForPaths["tyre2ModelPath"],
-              mapForPaths["terrainVertPath"], mapForPaths["terrainFragPath"],
-              mapForPaths["terrainModel1Path"], mapForPaths["terrainModel2Path"], mapForPaths["terrainModel3Path"]
-              );
-    game.initialize();
-
-    Shader skyboxShader(mapForPaths["skyVertPath"].c_str(), mapForPaths["skyFragPath"].c_str());
+unsigned int skyboxVAO,skyboxVBO,skyboxEBO;
+void intiSkybox(Shader& skyboxShader,Game& game){
     skyboxShader.Use();
     skyboxShader.setInt("skybox",0);
-    unsigned int skyboxVAO,skyboxVBO, skyboxEBO;
     glGenVertexArrays(1, &skyboxVAO);
     glGenBuffers(1, &skyboxVBO);
     glGenBuffers(1, &skyboxEBO);
@@ -143,7 +124,6 @@ int main() {
                     front,
                     back
             };
-
     glGenTextures(1, &game.cubeMapTexture);
     glBindTexture(GL_TEXTURE_CUBE_MAP, game.cubeMapTexture);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -154,61 +134,72 @@ int main() {
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
     // This might help with seams on some systems
     //glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
-    for (unsigned int i = 0; i < 6; i++)
-    {
+    for (unsigned int i = 0; i < 6; i++){
         int width, height, nrChannels;
         unsigned char* data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
-        if (data)
-        {
-            glTexImage2D
-                    (
-                            GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-                            0,
-                            GL_RGB,
-                            width,
-                            height,
-                            0,
-                            GL_RGB,
-                            GL_UNSIGNED_BYTE,
-                            data
-                    );
+        if (data){
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
             stbi_image_free(data);
         }
-        else
-        {
+        else{
             std::cout << "Failed to load texture: " << facesCubemap[i] << std::endl;
             stbi_image_free(data);
         }
     }
+}
+void renderSkybox(Shader& skyboxShader,Game& game){
+    // Since the cubemap will always have a depth of 1.0, we need that equal sign so it doesn't get discarded
+    glDepthFunc(GL_LEQUAL);
+
+    skyboxShader.Use();
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+    // We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
+    // The last row and column affect the translation of the skybox (which we don't want to affect)
+    view = glm::mat4(glm::mat3(glm::lookAt(GlobalVariables::camera.Position, GlobalVariables::camera.Position + GlobalVariables::camera.Front, GlobalVariables::camera.Up)));
+    projection = glm::perspective(glm::radians(45.0f), (float)GlobalVariables::scrWidth / GlobalVariables::scrHeight, 0.1f, 100.0f);
+    skyboxShader.setMat4("view",view);
+    skyboxShader.setMat4("projection",projection);
+    // Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
+    // where an object is present (a depth of 1.0f will always fail against any object's depth value)
+    glBindVertexArray(skyboxVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, game.cubeMapTexture);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
+    // Switch back to the normal depth function
+    glDepthFunc(GL_LESS);
+}
+int main() {
+    Init init;
+    GLFWwindow *window = Init::initializeWindow();
+    map<const string, string> mapForPaths = Init::initializeShadersAndModelsPaths();
+
+    MainMenu mainMenu(window);
+    mainMenu.initializeImGui();
+    mainMenu.show();
+    Game game(mapForPaths["carVertPath"], mapForPaths["carFragPath"],
+              mapForPaths["carOneModelPath"],
+              mapForPaths["carTwoModelPath"],
+              mapForPaths["carThreeModelPath"],
+              mapForPaths["tyre1ModelPath"],
+              mapForPaths["tyre2ModelPath"],
+              mapForPaths["terrainVertPath"], mapForPaths["terrainFragPath"],
+              mapForPaths["terrainModel1Path"], mapForPaths["terrainModel2Path"], mapForPaths["terrainModel3Path"]
+              );
+    game.initialize();
+
+    Shader skyboxShader(mapForPaths["skyVertPath"].c_str(), mapForPaths["skyFragPath"].c_str());
+
+    intiSkybox(skyboxShader,game);
     while (!glfwWindowShouldClose(window)) {
         Init::updateDeltaTime();
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         if (gameState == GAME) {
             renderGame(window,game,init,mainMenu);
-            // Since the cubemap will always have a depth of 1.0, we need that equal sign so it doesn't get discarded
-            glDepthFunc(GL_LEQUAL);
-
-            skyboxShader.Use();
-            glm::mat4 view = glm::mat4(1.0f);
-            glm::mat4 projection = glm::mat4(1.0f);
-            // We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
-            // The last row and column affect the translation of the skybox (which we don't want to affect)
-            view = glm::mat4(glm::mat3(glm::lookAt(GlobalVariables::camera.Position, GlobalVariables::camera.Position + GlobalVariables::camera.Front, GlobalVariables::camera.Up)));
-            projection = glm::perspective(glm::radians(45.0f), (float)GlobalVariables::scrWidth / GlobalVariables::scrHeight, 0.1f, 100.0f);
-            skyboxShader.setMat4("view",view);
-            skyboxShader.setMat4("projection",projection);
-            // Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
-            // where an object is present (a depth of 1.0f will always fail against any object's depth value)
-            glBindVertexArray(skyboxVAO);
-            glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_CUBE_MAP, game.cubeMapTexture);
-            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-            glBindVertexArray(0);
-
-            // Switch back to the normal depth function
-            glDepthFunc(GL_LESS);
-            mainMenu.renderImGui();
+            renderSkybox(skyboxShader, game);
             handleReturnToMenu(mainMenu);
         } else if(gameState == MENU || GlobalVariables::returnToMenuClicked) {
             Init::processInputForPreGame(window,game.chosenCarIndex);
